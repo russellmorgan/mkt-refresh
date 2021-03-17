995e504588c888912f3ccdf0feaab76f4316352d
Russell Morgan <russell.morgan@gmail.com>
2021-03-17 14:20:17 -0400

    Working through detail page with alpine data

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 window.addEventListener('DOMContentLoaded', function() {
   //var navElements = document.querySelectorAll("nav li > aside");
+  
 });
+
+function templates() {
+  return {
+    agendaIntro: "Agenda title text here",
+    agendas: [
+      {"title":"agenda 1","link":"https://www.google.com"},
+      {"title":"agenda 2","link":"email url"},
+      {"title":"agenda 3","link":"email url"},
+      {"title":"agenda 4","link":"email url"}
+    ],
+    emailIntro:"Email intro text placeholder Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    emails: [
+      {"title":"email 1","link":"https://www.google.com"},
+      {"title":"email 2","link":"email url"},
+      {"title":"email 3","link":"email url"},
+      {"title":"email 4","link":"email url"}
+    ],
+    presentationsIntro: "Presentations Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus."
+  }
+}
