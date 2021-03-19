8535e27c12e38484c2f73841fcfda789014ba0d8
Russell Morgan <russell.morgan@gmail.com>
2021-03-19 14:50:50 -0400

    All data in place and images for homepage

--- js/videos.js
+++ js/videos.js
@@ -2,16 +2,65 @@ function data() {
   return {
     showModal: false,
     currentVideo:{"title":"video title","link":"video.mp4","description":"lorem ipsum","length":"1:66"},
+    campusIntro:"Campus Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    campaignIntro:"Campaign Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    careerIntro:"Career Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    houseIntro:"House Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    tapIntro:"TAP Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
     videosWho: [
       {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
       {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
       {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
     ],
     videosRecruiting: [
-      {"title":"video title","link":"video.mp4"},
-      {"title":"video title","link":"video.mp4"},
-      {"title":"video title","link":"video.mp4"},
-      {"title":"video title","link":"video.mp4"}
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosWealth: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosResearch: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosInvestment: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosTech: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosSustainable: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosEquity: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosCareers: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosHouse: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
+    ],
+    videosTap: [
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"},
+      {"title":"video title","link":"video.mp4","description":"lorem ipsum desc","length":"1:66"}
     ]
   }
 }
