f1ff2d3efc7e911b3967523eb0cbec46a352afe6
Russell Morgan <russell.morgan@gmail.com>
2021-03-19 12:01:49 -0400

    Video modal styling

---
+++ js/videos.js
@@ -0,0 +1,33 @@
+function data() {
+  return {
+    showModal: false,
+    agendaIntro: "Agenda title text here. consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    agendas: [
+      {"title":"agenda 1","link":"https://www.google.com"},
+      {"title":"agenda 2","link":"email url"},
+      {"title":"agenda 3","link":"email url"},
+      {"title":"agenda 4","link":"email url"}
+    ],
+    emailIntro:"Email intro text placeholder Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    emails: [
+      {"title":"email 1","link":"https://www.google.com"},
+      {"title":"email 2","link":"email url"},
+      {"title":"email 3","link":"email url"},
+      {"title":"email 4","link":"email url"}
+    ],
+    presentationsIntro: "Presentations Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id magna vehicula, dictum elit pulvinar, convallis ipsum. Phasellus ut convallis tortor, elementum blandit lorem. Donec vel lectus elementum, luctus dui sed, volutpat augue. Phasellus eget turpis nec purus egestas maximus.",
+    videosWho: [
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"}
+    ],
+    videosRecruiting: [
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"},
+      {"title":"vdeo title","link":"video.mp4"}
+    ]
+  }
+}
